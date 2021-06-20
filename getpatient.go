3a7028181465faa91bc24e96e60a0b2ae25f1980
ABHISHEK SAREWAR <77223307+AbhishekSarewar1911@users.noreply.github.com>
2021-06-20 20:06:26 +0530

    Update getpatient.go

--- getpatient.go
+++ getpatient.go
@@ -18,8 +18,49 @@ func GetDoc(dB *sql.DB) gin.HandlerFunc {
 			utils.RespondWithError(http.StatusBadRequest, "invalid format", c)
 			return
 		}
+		
+		if search.Email == "" {
+		rows, err := dB.Query("SELECT p.patient_name, p.age, g.gender FROM patient p INNER JOIN user u ON u.user_id=p.user_id INNER JOIN gender_lookup g ON g.gender_id=p.gender WHERE u.email=?", search.Email, patientId)
+			if err != nil {
+				utils.RespondWithError(http.StatusInternalServerError, "sorry Patient not Found", c)
+				return
+			} else {
+				for rows.Next() {
+					err := rows.Scan(&patient.Name, &patient.Age, &patient.Gender)
+					if err != nil {
+						utils.RespondWithError(http.StatusInternalServerError, err.Error(), c)
+						return
+					}
+					patients = append(patients, patient)
+				}
+				c.JSON(http.StatusOK, gin.H{
+					"success": "true",
+					"message": "sent patients list successfully",
+					"Patients": patients,
+				})
+			}
+		}
+		
+		
+		
+		
     
-    if search.Key == "email" {
+   
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+/*             if search.Key == "email" {
 		var patientId int
 		errC := dB.QueryRow("SELECT p.patient_name, p.age, g.gender FROM patient p INNER JOIN user u ON u.user_id=p.user_id INNER JOIN gender_lookup g ON g.gender_id=p.gender WHERE u.email=?", search.Email).Scan(&patientId)
 		if errC == sql.ErrNoRows {
@@ -39,7 +80,7 @@ func GetDoc(dB *sql.DB) gin.HandlerFunc {
 					"message": patients,
 				})
 			}
-		}
+		} */
 	    
 	    
 	    
